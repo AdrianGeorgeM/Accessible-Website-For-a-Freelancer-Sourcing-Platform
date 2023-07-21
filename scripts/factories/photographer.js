0068d1d9b2acc552241bd15f9126e3afcb95862a
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-21 13:18:41 +0100

    Added classes to photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,20 +4,27 @@ function photographerFactory(data) {
 
 	function getUserCardDOM() {
 		const article = document.createElement('article');
+		article.classList.add('gallery-item');
+
 		const img = document.createElement('img');
 		img.setAttribute('src', picture);
+		img.classList.add('item-portrait');
 
 		const h2 = document.createElement('h2');
 		h2.textContent = name;
+		h2.classList.add('item-title');
 
 		const pCity = document.createElement('p');
 		pCity.textContent = city;
+		pCity.classList.add('item-city');
 
 		const pTagline = document.createElement('p');
 		pTagline.textContent = tagline;
+		pTagline.classList.add('item-tagline');
 
 		const pPrice = document.createElement('p');
 		pPrice.textContent = `$${price}/hour`;
+		pPrice.classList.add('photo-price');
 
 		const a = document.createElement('a');
 		a.href = `photographer.html?id=${id}`;
